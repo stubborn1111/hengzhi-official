d8bad81ff071939caac99542c557da093e1d11a6
wangyu-debug <70136801+wangyu-debug@users.noreply.github.com>
2021-06-01 21:28:51 +0800

    Update UserController.java
    非新股发行

--- src/main/java/com/hengzhi/controller/UserController.java
+++ src/main/java/com/hengzhi/controller/UserController.java
@@ -37,7 +37,7 @@ public class UserController {
     @ResponseBody
     @Security(false)
     public Map<String, String> login(@RequestBody User user, HttpServletResponse response) {
-        System.out.println("进来了ttttttt");
+        System.out.println("进来了tttttt");
         Map<String, String> map = new HashMap();
         map.put("status", "false");
         User userDB = userService.login(user);
