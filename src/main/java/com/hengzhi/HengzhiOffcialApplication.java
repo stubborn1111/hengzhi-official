673f663c23baa41ff15141cde53d821f6a9228fd
yang-yang-y <68273082+yang-yang-y@users.noreply.github.com>
2021-06-14 11:51:30 +0800

    Update HengzhiOffcialApplication.java

--- src/main/java/com/hengzhi/HengzhiOffcialApplication.java
+++ src/main/java/com/hengzhi/HengzhiOffcialApplication.java
@@ -5,15 +5,22 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
-public class HengzhiOffcialApplication {
+public class HengzhiOffcialApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
-
         SpringApplication.run(HengzhiOffcialApplication.class, args);
+    }
 
+    // 继承SpringBootServletInitializer 实现configure 方便打war 外部服务器部署。
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(HengzhiOffcialApplication.class);
     }
 
 }
