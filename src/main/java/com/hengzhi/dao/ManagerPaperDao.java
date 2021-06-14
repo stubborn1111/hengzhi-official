0f31f3ec6c644df6c32908df992ec0699acb9d99
wangyu-debug <70136801+wangyu-debug@users.noreply.github.com>
2021-06-14 12:15:45 +0800

    增加了试卷成绩信息功能
    试卷成绩信息

--- src/main/java/com/hengzhi/dao/ManagerPaperDao.java
+++ src/main/java/com/hengzhi/dao/ManagerPaperDao.java
@@ -1,6 +1,7 @@
 package com.hengzhi.dao;
 
 import com.hengzhi.dto.ManagerPaper.ChangePapers;
+import com.hengzhi.dto.ManagerPaper.ScoreInformation;
 import com.hengzhi.dto.ManagerPaper.UnChangePapers;
 import com.hengzhi.dto.ManagerPaper.UnFinishPapers;
 import org.apache.ibatis.annotations.Mapper;
@@ -28,4 +29,16 @@ public interface ManagerPaperDao {
      未考试卷信息
       */
      List<UnFinishPapers> selectUnFinish();
+     /*
+     考试试卷信息
+      */
+     List<ScoreInformation> selectScoreInformation(Integer paperId);
+     /*
+     计算每一张试卷的总分
+      */
+     int selectSumScore(Integer paperId);
+     /*
+     计算考试人数
+      */
+     int selectSumPeople(Integer paperId);
 }
--- src/main/java/com/hengzhi/service/ManagerPaperService.java
+++ src/main/java/com/hengzhi/service/ManagerPaperService.java
@@ -1,6 +1,7 @@
 package com.hengzhi.service;
 
 import com.hengzhi.dto.ManagerPaper.ChangePapers;
+import com.hengzhi.dto.ManagerPaper.ScoreInformation;
 import com.hengzhi.dto.ManagerPaper.UnChangePapers;
 import com.hengzhi.dto.ManagerPaper.UnFinishPapers;
 
@@ -23,4 +24,16 @@ public interface ManagerPaperService {
       未考试卷信息
        */
     List<UnFinishPapers> selectUnFinish();
+    /*
+     考试试卷信息
+      */
+    List<ScoreInformation> selectScoreInformation(Integer paperId);
+    /*
+    计算每一张试卷的总分
+     */
+    int selectSumScore(Integer paperId);
+    /*
+    计算考试人数
+     */
+    int selectSumPeople(Integer paperId);
 }
