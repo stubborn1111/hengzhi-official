ac855aaccee19706b67212ce6fd37ab03d7aea15
Rizwang233 <2380886380@qq.com>
2021-06-23 19:01:21 +0800

    bug

--- src/main/java/com/hengzhi/controller/ShowController.java
+++ src/main/java/com/hengzhi/controller/ShowController.java
@@ -13,7 +13,6 @@ import org.apache.shiro.authz.annotation.Logical;
 import org.apache.shiro.authz.annotation.RequiresRoles;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -27,104 +26,97 @@ import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
-
-@RestController
-@CrossOrigin(origins = "*", maxAge = 3600)
 @RequestMapping("/show")
+@RestController
+@CrossOrigin(origins = "*",maxAge = 3600)
 public class ShowController {
     @Autowired
     ShowService showService;
-
     @RequestMapping("/showMessages")
     @ResponseBody
     @Security(false)
-    public Map showMessages(@RequestBody JSONObject jsonObject) {
+    public Map showMessages(@RequestBody JSONObject jsonObject){
         Integer page = jsonObject.getInteger("page");
         Integer size = jsonObject.getInteger("size");
         Map map = new HashMap();
-        List<Message> list = showService.selectThroughMes(page, size);
+        List<Message> list = showService.selectThroughMes(page,size);
         Integer TotalNumber = showService.selectCountThroughMes();
-        Double all = Math.ceil((float) TotalNumber / size);
-        int allP = all.intValue();
-        if (page <= allP)
-            map.put("listMessage", list);
-        else map.put("listMessage", null);
-        map.put("TotalNumber", TotalNumber);
-        if (TotalNumber < size) {
-            map.put("pagesSize", 1);
-        } else if (TotalNumber % size == 0) {
-            map.put("pagesSize", TotalNumber / size);
-        } else {
-            map.put("pagesSize", TotalNumber / size + 1);
+        Double all= Math.ceil((float)TotalNumber/size);
+        int allP=all.intValue();
+        if(page<=allP)
+        map.put("listMessage",list);
+        else map.put("listMessage",null);
+        map.put("TotalNumber",TotalNumber);
+        if(TotalNumber<size){
+            map.put("pagesSize",1);
+        } else if(TotalNumber%size==0){
+            map.put("pagesSize",TotalNumber/size);
+        }else {
+            map.put("pagesSize",TotalNumber/size+1);
         }
-        map.put("page", page);
+        map.put("page",page);
         return map;
     }
-
-    //    增加留言
+//    增加留言
     @RequestMapping("/addMessages")
     @ResponseBody
     @Security(false)
     public HashMap addMessages(@RequestBody JSONObject jsonObject) {
-        String content = jsonObject.getString("content");
+        String content=jsonObject.getString("content");
         showService.InsertMessages(content);
-        HashMap map = new HashMap();
-        map.put("msg", "success");
+        HashMap map=new HashMap();
+        map.put("msg","success");
         return map;
     }
-
-    //    发布公告
+//    发布公告
     @ResponseBody
     @RequestMapping("/addNotice")
     @RequiresRoles(value = {"admin"})
-    public HashMap addNotice(@RequestBody JSONObject jsonObject) {
-        int userId = jsonObject.getInteger("userId");
-        String content = jsonObject.getString("content");
-        showService.insertNotice(content, userId);
-        HashMap map = new HashMap();
-        map.put("msg", "success");
+    public HashMap addNotice(@RequestBody JSONObject jsonObject){
+        int userId=jsonObject.getInteger("userId");
+        String content=jsonObject.getString("content");
+        showService.insertNotice(content,userId);
+        HashMap map=new HashMap();
+        map.put("msg","success");
         return map;
     }
-
-    //    显示公告
+//    显示公告
     @RequestMapping("/showNotice")
     @ResponseBody
     @Security(false)
-    public List<Notice> showNotice() {
+    public List<Notice> showNotice(){
         return showService.showNotice();
     }
-
     //    修改简介
     @ResponseBody
     @RequestMapping("/updateIntroduction")
     @RequiresRoles(value = {"super"})
-    public HashMap updateIntroduction(@RequestParam(value = "teamIntroduction") String teamIntroduction, @RequestParam(value = "front") String front, @RequestParam(value = "behind") String behind, HttpServletRequest request) {
-        showService.updateIntroduction(teamIntroduction, behind, front);
-        HashMap map = new HashMap();
-        map.put("msg", "success");
-        return map;
+    public HashMap updateIntroduction(@RequestParam(value = "teamIntroduction")String teamIntroduction,@RequestParam(value = "front")String front,@RequestParam(value = "behind")String behind, HttpServletRequest request){
+            showService.updateIntroduction(teamIntroduction,behind,front);
+            HashMap map=new HashMap();
+            map.put("msg","success");
+            return map;
     }
 
-    //    显示简介
+//    显示简介
     @RequestMapping("/showIntroduction")
     @ResponseBody
     @Security(false)
-    public Introduction showIntroduction() {
+    public Introduction showIntroduction(){
         return showService.showIntroduction();
     }
-
-    //    上传资料
+//    上传资料
     @ResponseBody
     @RequestMapping("/uploadFile")
     @RequiresRoles(value = {"admin"})
-    public HashMap uploadFile(@RequestParam(value = "userId") int userId, @RequestParam(value = "description") String description, @RequestParam(value = "file", required = false) MultipartFile file, HttpServletRequest request) {
-        HashMap map = new HashMap();
+    public HashMap uploadFile(@RequestParam(value = "userId")int userId,@RequestParam(value = "description")String description, @RequestParam(value = "file",required = false)MultipartFile file,HttpServletRequest request){
+        HashMap map=new HashMap();
         if (!file.isEmpty()) {
             String filePath = request.getServletContext().getRealPath("/file");
             System.out.println(request.getContextPath());
             String originalFilename = file.getOriginalFilename();
             // UUID随机重命名
-            String url = (UUID.randomUUID() + originalFilename
+           String url = (UUID.randomUUID() + originalFilename
                     .substring(originalFilename.indexOf("."))).replace("-", "");
             // 新文件
             File file1 = new File(filePath, url);
@@ -138,62 +130,51 @@ public class ShowController {
             } catch (IllegalStateException | IOException e) {
                 e.printStackTrace();
             }
-            showService.addFile(userId, description, url);
+            showService.addFile(userId,description,url);
 
-            map.put("msg", "success");
-        } else map.put("msg", "error");
+            map.put("msg","success");
+    }
+        else map.put("msg","error");
         return map;
     }
-
-    //    文件下载
+//    文件下载
     @RequestMapping("/downloadFile")
     @ResponseBody
-    @RequiresRoles(value = {"admin", "user", "super"}, logical = Logical.OR)
-    public HashMap downloadFile(@RequestParam(value = "fileName") String fileName, HttpServletResponse response, HttpServletRequest request) throws IOException {
-        if ("options".equals(request.getMethod())) {
-            response.setStatus(HttpStatus.OK.value());
-            return new HashMap();
-        }
+    @RequiresRoles(value = {"admin","user","super"}, logical = Logical.OR)
+    public HashMap downloadFile(@RequestParam(value = "fileName", required = false)String fileName, HttpServletResponse response,HttpServletRequest request) throws IOException {
         String filePath = request.getServletContext().getRealPath("/file");
-        File file = new File(filePath + "/" + fileName);
-        HashMap map = new HashMap();
+            File file = new File(filePath+"/"+fileName);
+            HashMap map=new HashMap();
         if (!file.exists()) {
             response.sendError(404, "File not found!");
-            map.put("msg", "error");
+            map.put("msg","error");
         }
         BufferedInputStream br = new BufferedInputStream(new FileInputStream(file));
         byte[] buf = new byte[1024];
         int len = 0;
 
         response.reset(); // 非常重要
-        response.setHeader("Access-control-Allow-Origin", "*");
-        response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS,PUT,DELETE");
-        response.setHeader("Access-Control-Expose-Headers", "Authorization");
-        response.setHeader("Access-Control-Allow-Headers", "Authorization,Content-Type,Access-Control-Expose-Headers");
-        //response.setContentType("application/octet-stream");
-        // 纯下载方式
-        response.setContentType("application/x-msdownload");
-        response.setHeader("Content-Disposition", "attachment; filename=" + file.getName());
+         // 纯下载方式
+            response.setContentType("application/x-msdownload");
+            response.setHeader("Content-Disposition", "attachment; filename=" + file.getName());
 
         OutputStream out = response.getOutputStream();
         while ((len = br.read(buf)) > 0)
             out.write(buf, 0, len);
         br.close();
         out.close();
-        map.put("msg", "success");
+        map.put("msg","success");
         return map;
     }
-
-    //    资料列表
+//    资料列表
     @RequestMapping("/showFile")
     @ResponseBody
-    @RequiresRoles(value = {"admin", "user", "super"}, logical = Logical.OR)
-    public List showFile() {
+    @RequiresRoles(value = {"admin","user","super"}, logical = Logical.OR)
+    public List showFile(){
         return showService.showFile();
     }
 
-}
+    }
 
 
 
--- src/main/java/com/hengzhi/secutity/BCryptPasswordEncoder.java
+++ src/main/java/com/hengzhi/secutity/BCryptPasswordEncoder.java
@@ -54,8 +54,6 @@ public class BCryptPasswordEncoder implements PasswordEncoder {
 
         return BCrypt.hashpw(rawPassword.toString(), salt);
     }
-
-
     /*
      *匹配密码具体实现
      */
